bc3fd043aaf73dae57281e2289cd0800c449fbc6
Jyri Huhtala <jyrgenhuhtala@gmail.com>
2024-07-16 13:42:28 +0300

    Suoritukset-luokkaan lisätty tietokantaversion vaatimat metodit ja muutokset. Suoritukset-luokasta poistettu tiedoston lukemiseen liittyvät metodit

--- HT/src/Siivoustiimi/Suoritukset.java
+++ HT/src/Siivoustiimi/Suoritukset.java
@@ -1,233 +1,130 @@
 package Siivoustiimi;
 
 import java.io.*;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 
-public class Suoritukset implements Iterable<Suoritus> {
-    private String tiedostonPerusNimi = "";
+import static Siivoustiimi.Kanta.alustaKanta;
 
-    private boolean muutettu = false;
+/**
+ * Siivoustiimin suoritukset, joka osaa mm. lisata uuden suorituksen
+ * @author jyrihuhtala
+ * @version 16.7.2024
+ */
 
-    private final Collection<Suoritus> alkiot = new ArrayList<Suoritus>();
+public class Suoritukset  {
 
-
-    /**
-     * Luodaan Arraylist suorituksista
-     */
-    public Suoritukset() {
-
-    }
-
+    private static Suoritus apuSuoritus = new Suoritus();
+    private Kanta kanta;
 
 
     /**
-     * Palauttaa kaikki yhden kotityön suoritukset ArrayList muodossa. Etsitään
-     * kotityön ID:n perusteella.
-     * @param id Kotityön id
-     * @return Palauttaa kaikki yhden kotityön suoritukset ArrayList muodossa.
-     * @example <pre name="test">
-     * #import java.util.*;
-     * Suoritukset suoritukset = new Suoritukset();
-     * Suoritus suoritus1 = new Suoritus(2); suoritukset.lisaa(suoritus1);
-     * Suoritus suoritus2 = new Suoritus(1); suoritukset.lisaa(suoritus2);
-     * Suoritus suoritus3 = new Suoritus(2); suoritukset.lisaa(suoritus3);
-     * Suoritus suoritus4 = new Suoritus(2); suoritukset.lisaa(suoritus4);
-     * Suoritus suoritus5 = new Suoritus(2); suoritukset.lisaa(suoritus5);
-     * Suoritus suoritus6 = new Suoritus(3); suoritukset.lisaa(suoritus6);
-     *
-     * List<Suoritus> loytyneet;
-     * loytyneet = suoritukset.annaSuoritukset(5);
-     * loytyneet.size() === 0;
-     * loytyneet = suoritukset.annaSuoritukset(1);
-     * loytyneet.size() === 1;
-     * loytyneet = suoritukset.annaSuoritukset(2);
-     * loytyneet.size() === 4;
-     * loytyneet.get(0) == suoritus1 === true;
-     * loytyneet.get(1) == suoritus3 === true;
-     * loytyneet = suoritukset.annaSuoritukset(2);
-     * loytyneet.size() === 4;
-     * </pre>
+     * Tarkistetaan, että tietokannassa on suoritusten tarvitsema taulu
+     * @param nimi tietokannan nimi
+     * @throws SailoException jos jokin menee pieleen
      */
-    public ArrayList<Suoritus> annaSuoritukset (int id) {
-        ArrayList<Suoritus> loydetyt = new ArrayList<>();
-        for (Suoritus suoritus : alkiot)
-            if (suoritus.getKotityoID() == id) loydetyt.add(suoritus);
-        return loydetyt;
-    }
-
-
-    /**Lisaa uuden suorituksen tietorakenteeseen.
-     * @param suoritus lisattava kotityo
-     * @example <pre name="test">
-     * Suoritukset suoritukset = new Suoritukset();
-     * Suoritus suoritus1 = new Suoritus(), suoritus2 = new Suoritus();
-     * suoritukset.getLkm() === 0;
-     * suoritukset.lisaa(suoritus1); suoritukset.getLkm() ===1;
-     * suoritukset.lisaa(suoritus2); suoritukset.getLkm() ===2;
-     * suoritukset.lisaa(suoritus1); suoritukset.getLkm() ===3;
-     * suoritukset.lisaa(suoritus1); suoritukset.getLkm() ===4;
-     * suoritukset.lisaa(suoritus1); suoritukset.getLkm() ===5;
-     * </pre>
-     */
-    public void lisaa (Suoritus suoritus) {
-        alkiot.add(suoritus);
-        muutettu = true;
-    }
-
-    /**
-     * Palauttaa siivoustiimin suoritusten lukumaaran
-     * @return suoritusten lukumaara
-     */
-    public int getLkm() {
-        return alkiot.size();
-    }
-
-    /**
-     * Asettaa tiedoston perusnimen ilman tarkenninta
-     * @param tied tallennustiedoston perusnimi
-     */
-    public void setTiedostonPerusNimi(String tied) {
-        tiedostonPerusNimi = tied;
-    }
-
-
-    /**
-     * Palauttaa tiedoston nimen, jota käytetään tallennukseen
-     * @return tallennustiedoston nimi
-     */
-    public String getTiedostonPerusNimi() {
-        return tiedostonPerusNimi;
-    }
-
-
-    /**
-     * Palauttaa tiedoston nimen, jota käytetään tallennukseen
-     * @return tallennustiedoston nimi
-     */
-    public String getTiedostonNimi() {
-        return tiedostonPerusNimi + ".dat";
-    }
-
-
-    /**
-     * Palauttaa varakopiotiedoston nimen
-     * @return varakopiotiedoston nimi
-     */
-    public String getBakNimi() {
-        return tiedostonPerusNimi + ".bak";
-    }
-
-
-    @Override
-    public Iterator<Suoritus> iterator() {
-        return alkiot.iterator();
-    }
-
-
-    /**
-     * Lukee suoritukset tiedostosta.
-     *
-     * @param tiedosto tiedoston nimi
-     * @throws SailoException jos lukeminen epäonnistuu
-     * @example
-     * <pre name="test">
-     * #THROWS SailoException
-     * #THROWS java.io.IOException
-     * #import java.io.*;
-     * #import java.util.Iterator;
-     * Suoritukset suoritukset = new Suoritukset();
-     * Suoritus suoritus21 = new Suoritus(); suoritus21.taytaSuoritus(2,1);
-     * Suoritus suoritus11 = new Suoritus(); suoritus11.taytaSuoritus(1,2);
-     * Suoritus suoritus22 = new Suoritus(); suoritus22.taytaSuoritus(2,1);
-     * Suoritus suoritus12 = new Suoritus(); suoritus12.taytaSuoritus(1,1);
-     * Suoritus suoritus23 = new Suoritus(); suoritus23.taytaSuoritus(2,2);
-     * String tiedNimi = "testiTiedosto";
-     * File ftied = new File(tiedNimi+".dat");
-     * ftied.delete();
-     * suoritukset.lueTiedostosta(tiedNimi); #THROWS SailoException
-     * suoritukset.lisaa(suoritus21);
-     * suoritukset.lisaa(suoritus11);
-     * suoritukset.lisaa(suoritus22);
-     * suoritukset.lisaa(suoritus12);
-     * suoritukset.lisaa(suoritus23);
-     *  try {
-     *           suoritukset.tallenna();
-     *       } catch (IOException e) {
-     *           throw new RuntimeException(e);
-     *       }
-     * suoritukset = new Suoritukset();
-     * suoritukset.lueTiedostosta(tiedNimi);
-     * Iterator<Suoritus> i = suoritukset.iterator();
-     * i.next().toString() === suoritus21.toString();
-     * i.next().toString() === suoritus11.toString();
-     * i.next().toString() === suoritus22.toString();
-     * i.next().toString() === suoritus12.toString();
-     * i.next().toString() === suoritus23.toString();
-     * i.hasNext() === false;
-     * suoritukset.lisaa(suoritus23);
-     *  try {
-     *           suoritukset.tallenna();
-     *       } catch (IOException e) {
-     *           throw new RuntimeException(e);
-     *       }
-     * ftied.delete() === true;
-     * File fbak = new File(tiedNimi+".bak");
-     * fbak.delete() === true;
-     * </pre>
-     */
-    public void lueTiedostosta(String tiedosto) throws SailoException {
-        setTiedostonPerusNimi(tiedosto);
-
-        try (BufferedReader fi = new BufferedReader(new FileReader(getTiedostonNimi()))) {
-
-            String rivi;
-            while ( (rivi = fi.readLine()) != null ) {
-                rivi = rivi.trim();
-                if ( "".equals(rivi) || rivi.charAt(0) == ';' ) continue;
-                Suoritus suoritus = new Suoritus();
-                suoritus.parse(rivi);
-                lisaa(suoritus);
+    public Suoritukset(String nimi) throws SailoException, SQLException {
+        kanta = alustaKanta(nimi);
+        try (Connection con = kanta.annaKantayhteys() ) {
+            DatabaseMetaData meta = con.getMetaData();
+            try (ResultSet taulu = meta.getTables(null, null, "Suoritukset", null)) {
+                if (!taulu.next()) {
+                    try (PreparedStatement sql = con.prepareStatement(apuSuoritus.annaLuontiLauseke())) {
+                        sql.execute();
+                    }
+                }
             }
-            muutettu = false;
-
-        } catch ( FileNotFoundException e ) {
-            throw new SailoException("Tiedosto " + getTiedostonNimi() + " ei aukea");
-        } catch ( IOException e ) {
-            throw new SailoException("Ongelmia tiedoston kanssa: " + e.getMessage());
+        } catch (SQLException e) {
+            throw new SailoException("Ongelmia tietokannan kanssa:" + e.getMessage());
         }
     }
 
     /**
-     *  Luetaan aikaisemmin annetun nimisestä tiedostosta
-     * @throws SailoException jos tulee poikkeus
+     * Lisataan uusi suoritus siivoustiimiin
+     * @param suoritus lisattava suoritus
+     * @throws SailoException jos tietokantayhteyden kanssa ongelmia
      */
-    public void lueTiedostosta() throws SailoException, FileNotFoundException {
-        lueTiedostosta(getTiedostonPerusNimi());
+    public void lisaa(Suoritus suoritus) throws SailoException {
+        try (Connection con = kanta.annaKantayhteys();
+             PreparedStatement sql = suoritus.annaLisayslauseke(con)) {
+            sql.executeUpdate();
+            try (ResultSet rs = sql.getGeneratedKeys()){
+                suoritus.tarkistaId(rs);
+            }
+        }catch (SQLException e) {
+            throw new SailoException("Ongelmia tietokannan kanssa:" + e.getMessage());
+        }
     }
 
+
     /**
-     * Tallentaa suoritukset tiedostoon.
-     * @throws SailoException jos tallennus epäonnistuu
+     * Haetaan kaikki kotityon suoritukset
+     * @return Arraylist tietorakenne, jossa viitteet löydettyihin suorituksiin
+     * @throws SailoException jos suoritusten hakeminen tietokannasta epaonnistuu
      */
-    public void tallenna() throws SailoException, IOException {
-        if ( !muutettu ) return;
-        File fbak = new File(getBakNimi());
-        File ftied = new File(getTiedostonNimi());
+    public ArrayList<Suoritus> annaSuoritukset (int kotityoID) throws SailoException {
+       ArrayList<Suoritus> loydetyt = new ArrayList<>();
 
-        fbak.delete();
-        ftied.renameTo(fbak);
+       try (Connection con = kanta.annaKantayhteys();
+            PreparedStatement sql = con.prepareStatement("SELECT * FROM Suoritukset WHERE kotityoID = ?")
+            ) {
+           sql.setInt(1, kotityoID);
+           try (ResultSet tulokset = sql.executeQuery() ) {
+               while (tulokset.next()) {
+                   Suoritus suoritus = new Suoritus();
+                   suoritus.parse(tulokset);
+                   loydetyt.add(suoritus);
+               }
+           }
 
-        try ( PrintWriter fo = new PrintWriter(new FileWriter(ftied.getCanonicalPath())) ) {
-            for (Suoritus suoritus : this) {
-                fo.println(suoritus.toString());
+       } catch (SQLException e) {
+           throw new SailoException("Ongelmia tietokannan kanssa:" + e.getMessage());
+       }
+       return loydetyt;
+    }
+
+
+
+    /**
+     * Testiohjelma suorituksille
+     * @param args ei kaytossa
+     */
+    public static void main(String[] args)  {
+        try {
+            Suoritukset suoritukset = new Suoritukset("kokeilu");
+            Suoritus testiSuoritus1 = new Suoritus();
+            testiSuoritus1.taytaSuoritus(1,2);
+            Suoritus testiSuoritus2 = new Suoritus();
+            testiSuoritus2.taytaSuoritus(1,2);
+            Suoritus testiSuoritus3 = new Suoritus();
+            testiSuoritus3.taytaSuoritus(1,2);
+            Suoritus testiSuoritus4 = new Suoritus();
+            testiSuoritus4.taytaSuoritus(1,2);
+
+            suoritukset.lisaa(testiSuoritus1);
+            suoritukset.lisaa(testiSuoritus2);
+            suoritukset.lisaa(testiSuoritus3);
+            suoritukset.lisaa(testiSuoritus2);
+            suoritukset.lisaa(testiSuoritus4);
+
+            System.out.println("============= Harrastukset testi =================");
+
+            ArrayList<Suoritus> suoritukset2;
+
+            suoritukset2 = suoritukset.annaSuoritukset(2);
+
+            for (Suoritus suoritus : suoritukset2) {
+                System.out.print(suoritus.getKotityoID() + " ");
+                suoritus.tulosta(System.out);
             }
-        }catch (FileNotFoundException e) {
-            throw new SailoException("Tiedosto " + ftied.getName() + " ei aukea");
-        } catch ( IOException ex ) {
-            throw new SailoException("Tiedoston " + ftied.getName() + " kirjoittamisessa ongelmia");
+
+            new File("kokeilu.db").delete();
+        } catch (SailoException ex) {
+            System.out.println(ex.getMessage());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
-        muutettu = false;
     }
 }
