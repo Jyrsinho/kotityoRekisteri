b057443ea400823c4c7501134cd0db511e10a3a4
Jyri Huhtala <jyrgenhuhtala@gmail.com>
2024-07-20 18:34:40 +0300

    Testi Jasen luokan Parse(ResultSet) -metodille

--- HT/src/main/java/Siivoustiimi/test/JasenTest.java
+++ HT/src/main/java/Siivoustiimi/test/JasenTest.java
@@ -9,9 +9,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 import Siivoustiimi.*;
-
-import javax.sql.DataSource;
-import java.sql.Connection;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.when;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 
@@ -19,6 +19,7 @@ public class JasenTest {
 
   private Jasen timo1;
   private Kanta kanta;
+
 
   @BeforeEach
   public void alustus() {
@@ -58,14 +59,32 @@ public class JasenTest {
   @Test
   public void testinPitaisParsiaJasenResultsetista() throws SQLException {
 
+    //Määritellään mockattu Resultset ja sen käyttäytyminen
+    ResultSet resultSet = Mockito.mock(ResultSet.class);
+    when(resultSet.next()).thenReturn(true).thenReturn(false);
+    when(resultSet.getString("sukunimi")).thenReturn("Hemminki");
+    when(resultSet.getString("etunimi")).thenReturn("Seppo");
+    when(resultSet.getString("katuosoite")).thenReturn("Vilhontie 4");
+    when(resultSet.getString("postinumero")).thenReturn("00100");
+    when(resultSet.getString("kaupunki")).thenReturn("Vantaa");
+    when(resultSet.getString("puhelinNumero")).thenReturn("0400100");
+    when(resultSet.getInt("ika")).thenReturn(33);
 
+    // Käytetään mockattua Resultsetiä
+    while (resultSet.next()) {
+      timo1.parse(resultSet);
+    }
 
+    //VArmistetaan arvot
+    assertEquals("Hemminki", timo1.getSukunimi());
+    assertEquals("Seppo", timo1.getEtunimi());
+    assertEquals("Vilhontie 4", timo1.getKatuosoite());
+    assertEquals("00100", timo1.getPostinumero());
+    assertEquals("Vantaa", timo1.getKaupunki());
+    assertEquals("0400100", timo1.getPuhelin());
+    assertEquals(33, timo1.getIka());
 
-
-
+    // varmistetaan että next()iä kutsuttiin kaksi kertaa.
+    Mockito.verify(resultSet, Mockito.times(2)).next();
   }
-
-
-
-
 }
